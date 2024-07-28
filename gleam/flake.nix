111d998523dc9f63cc048813ea38d9d25ff1d823
Anuj More <anujmorex@gmail.com>
2024-07-28 23:00:34 +0800

    Ignore Hex and Mix folders for Gleam and other BEAM languages

--- gleam/flake.nix
+++ gleam/flake.nix
@@ -1,38 +1,102 @@
 {
-  description = "Developer environment shell for Ruby";
+  description = "A Nix-flake-based Gleam development environment";
 
-  inputs = {
-    # Nixpkgs: branch: nixos-unstable
-    # Updated: 15th July 2024
-    nixpkgs = {
-      type = "github";
-      owner = "NixOS";
-      repo = "nixpkgs";
-      rev = "693bc46d169f5af9c992095736e82c3488bf7dbb";
-    };
-  };
+  # inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+  # Nixpkgs: Branch: Nixos-unstable on 15th July 2024
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/693bc46d169f5af9c992095736e82c3488bf7dbb";
+  # inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
 
   outputs = { self, nixpkgs }:
     let
-      # Helper to provide system-specific attributes
-      forAllSupportedSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlays.default ];
+        };
       });
-
-      supportedSystems = [
-        "aarch64-darwin"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "x86_64-linux"
-      ];
     in
-
     {
-      devShells = forAllSupportedSystems ({ pkgs }: {
+      overlays.default = final: prev: rec {
+        # documentation
+        # https://nixos.org/manual/nixpkgs/stable/#sec-beam
+
+        # ==== ERLANG ====
+
+        # use whatever version is currently defined in nixpkgs
+        erlang = final.beam.interpreters.erlang;
+
+        # use latest version of Erlang 27
+        # erlang = final.beam.interpreters.erlang_27;
+
+        # specify exact version of Erlang OTP
+        # erlang = pkgs.beam.interpreters.erlang.override {
+        #   version = "26.2.2";
+        #   sha256 = "sha256-7S+mC4pDcbXyhW2r5y8+VcX9JQXq5iEUJZiFmgVMPZ0=";
+        # }
+
+        # ==== BEAM packages ====
+
+        # all BEAM packages will be compile with your preferred erlang version
+        pkgs-beam = final.beam.packagesWith erlang;
+
+        # ==== Gleam ====
+
+        # use whatever version is currently defined in nixpkgs
+        # gleam = pkgs-beam.gleam;
+
+        # use latest version of Gleam
+        gleam = pkgs-beam.gleam;
+
+        # specify exact version of Gleam
+        # gleam = pkgs-beam.gleam.override {
+        #   version = "1.17.1";
+        #   sha256 = "sha256-a7A+426uuo3bUjggkglY1lqHmSbZNpjPaFpQUXYtW9k=";
+        # };
+      };
+
+      devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
           packages = with pkgs; [
+            # use the Elixr/OTP versions defined above; will also install OTP, mix, hex, rebar3
             gleam
-          ];
+
+            # mix needs it for downloading dependencies
+            git
+
+            # probably needed for your Phoenix assets
+            nodejs_20
+          ]
+          ++
+          # Linux only
+          pkgs.lib.optionals pkgs.stdenv.isLinux (with pkgs; [
+            gigalixir
+            inotify-tools
+            libnotify
+          ])
+          ++
+          # macOS only
+          pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs; [
+            terminal-notifier
+            darwin.apple_sdk.frameworks.CoreFoundation
+            darwin.apple_sdk.frameworks.CoreServices
+          ]);
+
+          shellHook = ''
+            # this allows mix to work on the local directory
+            mkdir -p .nix-mix .nix-hex
+            export MIX_HOME=$PWD/.nix-mix
+            export HEX_HOME=$PWD/.nix-hex
+
+            # make hex from Nixpkgs available
+            # `mix local.hex` will install hex into MIX_HOME and should take precedence
+            export MIX_PATH="${(pkgs.beam.packagesWith pkgs.beam.interpreters.erlang).hex}/lib/erlang/lib/hex/ebin"
+            export PATH=$MIX_HOME/bin:$HEX_HOME/bin:$PATH
+
+            # keep shell history in iex
+            export ERL_AFLAGS="-kernel shell_history enabled"
+          '';
+
         };
       });
     };
