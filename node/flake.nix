7c21eb565719c762e8531e413336d49cec090cb1
Anuj More <anujmorex@gmail.com>
2024-09-12 19:23:16 +0800

    Add Go, Haskell, Java, Nix, Node, Ocaml, Purescript, Rust from the-nix-way/dev-templates

---
+++ node/flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "A Nix-flake-based Node.js development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+
+  outputs = { self, nixpkgs }:
+    let
+      # Helper to provide system-specific attributes
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSupportedSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      }); 
+    in
+    {
+      overlays.default = final: prev: rec {
+        nodejs = prev.nodejs;
+        yarn = (prev.yarn.override { inherit nodejs; });
+      };
+
+      devShells = forAllSupportedSystems ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            node2nix
+            nodejs
+            nodePackages.pnpm
+            yarn
+          ];
+        };
+      });
+    };
+}
