afee2ddc86b5f96bc34c0bcbd1e1b4c364e6b0cf
Anuj More <anujmorex@gmail.com>
2024-09-12 19:04:37 +0800

    Move Python to standard setup

--- python/flake.nix
+++ python/flake.nix
@@ -1,32 +1,27 @@
 {
-  description = "Nix Development Flake for your package";
+  description = "Developer environment shell for Python";
 
   inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
 
-  outputs =
-    { self, nixpkgs }:
+  outputs = { self, nixpkgs }:
     let
       # Helper to provide system-specific attributes
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forAllSupportedSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; };
-      });
-    in
-    flake-utils.lib.eachDefaultSystem
-      (system:
-      let
+      forAllSupportedSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f rec {
         pkgs = import nixpkgs { inherit system; };
         python = pkgs.python313;
         pythonPackages = python.pkgs;
-        lib-path = with pkgs; lib.makeLibraryPath [
+        libPaths = with pkgs; lib.makeLibraryPath [
           libffi
           openssl
           stdenv.cc.cc
         ];
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          name = "your_package";
+      });
+    in
+    {
+      devShells = forAllSupportedSystems ({ pkgs, python, pythonPackages, libPaths }: {
+        default = pkgs.mkShell {
+          name = "Python Shell";
           nativeBuildInputs = [ pkgs.bashInteractive ];
           buildInputs = with pythonPackages; [
             pkgs.poetry
@@ -45,8 +40,9 @@
             unset LD_PRELOAD
 
             PYTHONPATH=$PWD/$venvDir/${python.sitePackages}:$PYTHONPATH
-            export "LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${lib-path}"
+            export "LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${libPaths}"
           '';
         };
       });
+    };
 }
